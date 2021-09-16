5243581a8ad9db85b836c77d8c746ebbdc73ba7b
dvlplf <dvlplf@gmail.com>
2021-09-17 01:27:04 +0300

    Day 2. Task 1

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num >= 1 && num <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (num >= 5 && num <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (num >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
