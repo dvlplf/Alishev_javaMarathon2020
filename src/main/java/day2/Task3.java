1bc38f23fea9f021aace7b7ddb6b48627c05d8ef
dvlplf <dvlplf@gmail.com>
2021-09-17 01:57:57 +0300

    Day 2. Task 3

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int i = a + 1;
+        int b = scanner.nextInt();
+        while (i < b) {
+            if (i % 5 == 0) {
+                if (i % 10 == 0) {
+                    i++;
+                    continue;
+                }
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            i++;
+        }
     }
 }
