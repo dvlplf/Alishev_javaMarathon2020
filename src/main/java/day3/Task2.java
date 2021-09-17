c04f35028c256f8a3c3f07380e7e9fd8bb174b99
А336 <dvlplf@gmail.com>
2021-09-17 15:38:39 +0300

    day 3. Task 2 Test not success

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                break;
+            }
+            System.out.print(a / b);
+        }
     }
 }
