c6029caeb5559c80ec1f18c235dad2496ab54f44
А336 <dvlplf@gmail.com>
2021-09-17 17:12:36 +0300

    day 4. Task 4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,26 @@
 package day4;
 
+import java.util.Arrays;
+
 public class Task4 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+        int max = 0;
+        int idMax = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10000);
+        }
+        for (int i = 1; i < array.length - 1; i++) {
 
+            int sum = array[i - 1] + array[i] + array[i + 1];
+            if (sum > max) {
+                max = sum;
+                idMax = i - 1;
+            }
+        }
+        System.out.println("Массив:");
+        System.out.println(Arrays.toString(array));
+        System.out.println("sumMax = " + max);
+        System.out.println("idMax = " + idMax);
     }
 }
