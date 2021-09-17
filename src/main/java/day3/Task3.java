03e62f7095245c03e502f9eae107cc87df9573ea
А336 <dvlplf@gmail.com>
2021-09-17 15:47:14 +0300

    day 3. Task 3 Test not success

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        for (int i = 5; i > 0; i--) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
