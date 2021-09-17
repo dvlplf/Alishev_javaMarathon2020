8c29da5723d12e1105bfccd7551d39b9c477d67d
А336 <dvlplf@gmail.com>
2021-09-17 17:19:13 +0300

    day 5. Task 1 not ending

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -5,3 +5,33 @@ public class Task1 {
 
     }
 }
+
+class Car {
+    private int yearRelease;
+    private String color;
+    private String model;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearRelease() {
+        return yearRelease;
+    }
+
+    public void setYearRelease(int yearRelease) {
+        this.yearRelease = yearRelease;
+    }
+}
