cc938419f34428eb06063f86ba08e05ff3dde803
А336 <dvlplf@gmail.com>
2021-09-17 17:42:10 +0300

    day 5. Task 2

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,6 +2,34 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike harley = new Motorbike(1990, "Harley", "Black");
+        System.out.println(harley.getYear());
+        System.out.println(harley.getModel());
+        System.out.println(harley.getColor());
 
+    }
+}
+class Motorbike {
+    private int year;
+    private String model;
+    private String color;
+
+
+    public Motorbike(int year, String model, String color) {
+        this.year = year;
+        this.model = model;
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
