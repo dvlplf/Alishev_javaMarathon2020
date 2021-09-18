77bdf4b62a29d9412eb26ca7492eb0bedd48678c
dvlplf <dvlplf@gmail.com>
2021-09-18 22:38:31 +0300

    Day 7. Task 1

---
+++ src/main/java/day7/Airplane.java
@@ -0,0 +1,74 @@
+package day7;
+
+public class Airplane {
+    private String producer;
+    private int yearRelease;
+    private int length;
+    private int weight;
+    private int fuel = 0;
+
+    public Airplane(String producer, int yearRelease, int length, int weight) {
+        this.producer = producer;
+        this.yearRelease = yearRelease;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + this.producer+ ", год выпуска: " + this.yearRelease + ", длина: "+ this.length + ", вес: "+ this.weight +", количество топлива в баке: " + this.fuel);
+    }
+
+    public void fillUp(int addFuel) {
+        this.fuel += addFuel;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getYearRelease() {
+        return yearRelease;
+    }
+
+    public void setYearRelease(int yearRelease) {
+        this.yearRelease = yearRelease;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return this.fuel;
+    }
+
+    public static void compareAirplanes(Airplane airplane1, Airplane airplane2) {
+        if (airplane1.length == airplane2.length) {
+            System.out.println("Самолеты равны по длинне");
+        } else if (airplane1.length > airplane2.length) {
+            System.out.println(airplane1.producer + " длиннее, чем " + airplane2.getProducer());
+        } else {
+            System.out.println(airplane2.producer + " длиннее, чем " + airplane1.getProducer());
+        }
+    }
+}
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -2,6 +2,14 @@ package day7;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        Player player5 = new Player();
+        Player player6 = new Player();
+        Player player7 = new Player();
+        Player player8 = new Player();
+        Player.info();
     }
 }
