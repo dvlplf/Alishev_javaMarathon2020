8791184029e4640864eecbdcf41edbdc81cc7be6
dvlplf <dvlplf@gmail.com>
2021-09-19 13:08:27 +0300

    Day 8. Task 2

--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -1,7 +1,11 @@
 package day8;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane1 = new Airplane("Boeng", 1990, 100, 20000);
+        System.out.println(airplane1.toString());
     }
 }
