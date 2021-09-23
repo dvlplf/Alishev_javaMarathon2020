517b49476f978432e32139d7cd7b48f5ea81a88d
dvlplf <dvlplf@gmail.com>
2021-09-23 23:34:49 +0300

    Day 14. Task 1 Not ending

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,26 @@
 package day14;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/java/day14/.txt");
+        printSumDigits(file);
+
+    }
+
+    public static  void printSumDigits(File file) throws FileNotFoundException {
+        int res = 0;
+        Scanner sc = new Scanner(file);
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbersStr = line.split(" ");
+        for (String str: numbersStr) {
+            res += Integer.parseInt(str);
+        }
+        System.out.println(res);
     }
 }
