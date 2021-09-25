19db41d18f0f214381c0979f3168b6ccfd5dff10
dvlplf <dvlplf@gmail.com>
2021-09-25 21:05:33 +0300

    Day 14. Task 3

--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,64 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        File file = new File("src/main/java/day14/people.txt");
+        System.out.println(parseFileToObjList(file));
+    }
 
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> persons = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] strPerson = line.split(" ");
+                if (Integer.parseInt(strPerson[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                persons.add(new Person(strPerson[0], Integer.parseInt(strPerson[1])));
+            }
+            return persons;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+
+        return null;
+    }
+
+}
+
+class Person {
+    private String name;
+    private int year;
+
+    public Person(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
     }
 }
