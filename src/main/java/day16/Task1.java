cd689c77a07704adce04cde1743e3269640ca0a2
dvlplf <dvlplf@gmail.com>
2021-09-25 23:03:16 +0300

    Day 15. Task 1

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,32 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/java/day16/.txt");
+        printResult(file);
+    }
 
+    public static  void printResult(File file) {
+        int sum = 0;
+
+        try {
+            Scanner sc = new Scanner(file);
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbersStr = line.split(" ");
+            for (String str: numbersStr) {
+                sum += Integer.parseInt(str);
+            }
+
+            double result = (double) sum / numbersStr.length;
+            System.out.print(result + " --> " +String.format("%.3f", result));
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
