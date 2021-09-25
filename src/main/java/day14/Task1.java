729a6ba8df9b32eb38d6647584488fd78bd8574a
dvlplf <dvlplf@gmail.com>
2021-09-25 19:41:28 +0300

    Day 14. Task 1

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -2,6 +2,7 @@ package day14;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,15 +13,26 @@ public class Task1 {
 
     }
 
-    public static  void printSumDigits(File file) throws FileNotFoundException {
+    public static  void printSumDigits(File file) {
         int res = 0;
-        Scanner sc = new Scanner(file);
-        Scanner scanner = new Scanner(file);
-        String line = scanner.nextLine();
-        String[] numbersStr = line.split(" ");
-        for (String str: numbersStr) {
-            res += Integer.parseInt(str);
+        try {
+            Scanner sc = new Scanner(file);
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbersStr = line.split(" ");
+            if (numbersStr.length != 10) {
+                throw new IOException();
+            }
+            for (String str: numbersStr) {
+                res += Integer.parseInt(str);
+            }
+            System.out.println(res);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Некорректный входной файл");
         }
-        System.out.println(res);
+
+
     }
 }
