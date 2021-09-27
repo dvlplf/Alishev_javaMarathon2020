e129ec6cc80330ba83f99b8aa72cd33d6cf2923e
А336 <dvlplf@gmail.com>
2021-09-27 13:28:13 +0300

    Day 17. 1 task

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,22 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> carBrands = new ArrayList<>();
+        carBrands.add("Honda");
+        carBrands.add("Toyota");
+        carBrands.add("Ford");
+        carBrands.add("BMW");
+        carBrands.add("Kia");
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(carBrands.get(i));
+        }
+        carBrands.remove(0);
+        System.out.println(Arrays.toString(carBrands.toArray()));
     }
 }
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,51 @@
 package day17;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        String[] pieses = new String[8];
+        for (int i = 0; i < pieses.length; i++) {
+            if (i < 4) {
+                pieses[i] = ChessPiese.PAWN_WHITE.getStrImage();
+                continue;
+            }
+            pieses[i] = ChessPiese.ROOK_BLACK.getStrImage();;
+        }
+        for (String item: pieses) {
+            System.out.print(item + " ");
+        }
+    }
+}
 
+enum ChessPiese {
+    KING_WHITE("♔", 100),
+    KING_BLACK("♚", 100),
+    QUEEN_WHITE("♕", 9),
+    QUEEN_BLACK("♛", 9),
+    ROOK_WHITE("♖", 5),
+    ROOK_BLACK("♜", 5),
+    BISHOP_WHITE("♗", 3.5),
+    BISHOP_BLACK("♝", 3.5),
+    KNIGHT_WHITE("♘", 3),
+    KNIGHT_BLACK("♞", 3),
+    PAWN_WHITE("♙", 1),
+    PAWN_BLACK("♟", 1);
+
+    private String strImage;
+    private double value;
+
+    ChessPiese(String strImage, double value) {
+        this.strImage = strImage;
+        this.value = value;
+    }
+
+    public String getStrImage() {
+        return strImage;
+    }
+
+    public double getValue() {
+        return value;
     }
 }
