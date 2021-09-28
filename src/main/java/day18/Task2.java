018116b0b9bbbd0bf3325b54da57017eb2b3fc20
dvlplf <dvlplf@gmail.com>
2021-09-28 23:45:28 +0300

    Day 18. Task 2

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -5,10 +5,17 @@ package day18;
  */
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(count7(717771237)); // 5
+
+       System.out.println(count7(717771237));// 5
     }
 
     public static int count7(int number) {
+        if (number % 10 == 7) {
+            return 1 + count7(number / 10);
+        }
+        if (number > 0) {
+            return count7(number / 10);
+        }
        return 0;
     }
 }
