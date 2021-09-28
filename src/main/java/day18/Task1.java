5e067e7d247af5ef9b462fd22a1989ae73135b02
dvlplf <dvlplf@gmail.com>
2021-09-28 23:36:43 +0300

    Day 18. Task 1

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -4,9 +4,13 @@ public class Task1 {
     public static void main(String[] args) {
         int[] numbers = {-402, -50, -249, -15, -665, -311};
 
-    }
+        System.out.println(recursionSum(numbers, 0));
+       }
 
-    public static int recursionSum(int[] numbers, int i) {
+    public static int recursionSum (int[] numbers, int id) {
+        if (id < numbers.length)
+           return numbers[id] + recursionSum(numbers, id + 1);
+
         return 0;
     }
 }
