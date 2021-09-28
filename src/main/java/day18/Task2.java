76758f81bae91d11dc0c97c274d1a197869cac79
dvlplf <dvlplf@gmail.com>
2021-09-28 21:44:21 +0300

    Day 18. Clear tasks

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -9,12 +9,6 @@ public class Task2 {
     }
 
     public static int count7(int number) {
-        if (number == 0)
-            return 0;
-
-        if (number % 10 == 7)
-            return 1 + count7(number / 10);
-        else
-            return count7(number / 10);
+       return 0;
     }
 }
